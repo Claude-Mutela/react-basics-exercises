78e51b26c049d2066e7c8906f86a73002aad015f
Claude Mutela <claudemutela9@gmail.com>
2026-07-04 16:41:01 +0200

    feat(props): ajouter le composant Button typé (exercice 1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import './App.css'
-
+import Button from './components/Button'
 function App() {
 
   return (
     <>
       <section id="center">
-        
+        <Button label="Enregistrer" />
+        <Button label="Annuler" />
       </section>
-
       <div className="ticks"></div>
       <section id="spacer"></section>
     </>
---
+++ src/components/Button.tsx
@@ -0,0 +1,10 @@
+interface ButtonProps {
+    label: string;
+}
+ const Button = ({label}: ButtonProps) => {
+    return (
+        <button>{label}</button>
+    )
+}
+
+export default Button
